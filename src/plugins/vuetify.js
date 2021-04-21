ce9ad2258382d46e5ce0bcc14c6344ea4810ff43
Nick Hand <nicholas.adam.hand@gmail.com>
2021-04-21 13:24:46 -0400

    add vuetify

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,14 @@
+import { mdiCheckboxMarked, mdiCheckboxBlankOutline } from "@mdi/js"
+import Vue from 'vue'
+import Vuetify from 'vuetify/lib'
+import 'vuetify/dist/vuetify.min.css'
+
+Vue.use(Vuetify)
+
+
+
+export default new Vuetify({
+    icons: {
+        iconfont: 'mdiSvg',
+    },
+})
